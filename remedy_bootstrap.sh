3571f5231c2dd78be774dbcca4e16a5fdf102e3e
Dhmsbr <dhmsbr@gmail.com>
2013-05-23 18:24:41 +0200

    Create remedy_bootstrap.sh

---
+++ remedy_bootstrap.sh
@@ -0,0 +1,17 @@
+# install ooni-backend (+ own tor hidden service)
+
+# apt-get install collectd
+# setup collectd <salt/server/collectd/collectd.conf>
+# specific setup for aggregating data from ooni-backend <...>
+
+# install webserver <salt/server/webserver/nginx.conf>
+# install ssl <salt/server/webserver/ssl/ssl.cert & ssl.key>
+# install mysql
+# install php (+ dependencies)
+# setup php_rrdtool 
+
+# install drush
+# install drush_make
+# drush make <salt/server/dashboard/profiles/remedy.make>
+
+# install tor hidden service for dashboard <salt/server/tor/torrc>
