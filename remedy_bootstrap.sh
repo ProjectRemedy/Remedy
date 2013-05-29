b78cc46881dfb7be651d12809d9089e678eb7764
KheOps <kheops@ceops.eu>
2013-05-29 16:21:08 +0200

    Move remedy_bootstrap.sh back to root folder and add actual commands:   * clone the Remedy repo with git   * install salt-minion with aptitude (note: this will need a lot of improvements such as distro detection and setup of salt repos)   * copy state tree files from repo directory to /srv/salt

--- remedy_bootstrap.sh
+++ remedy_bootstrap.sh
@@ -5,8 +5,18 @@
 git clone git@github.com:ProjectRemedy/Remedy.git
 
 # add salt repositories - depends on distro!
+
 # install salt-minion
 aptitude install salt-minion
 
+# Create a local /srv directory if it does not exist
+if [ ! -e /srv ]; then
+  mkdir /srv
+fi
+
+# copy local srv/salt to filesystem
+cp -Ri Remedy/salt/srv/salt /srv/
+
+# At this time the one-time minion config file is in Remedy/salt/
 # Run something like:  salt-call --local state.highstate
 
