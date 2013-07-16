a81248d6c0e410e2b5d96ff2af7c2e55c0be4953
Dhmsbr <dhmsbr@gmail.com>
2013-07-16 12:59:09 +0000

    Adding patchied version of Bundle Inherit in order to add support for features : https://drupal.org/node/1870018

---
+++ salt/srv/salt/server/drupal/modules/bundle_inherit/bundle_inherit.api.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Bundle Inherit module.
+ */
+
+/**
+ *
+ * @param $entity_type
+ *   Entity type.
+ * @param $bundle
+ *   The name of the bundle to inherit.
+ * @param $bundle_parent
+ *   The name of the parent bundle to inherit from.
+ * @param $strict
+ *   Whether performed inheritance is strict or not.
+ */
+function hook_bundle_inherit_perform($entity_type, $bundle, $bundle_parent, $strict = TRUE) {
+  // TODO: Provide some example.
+}
