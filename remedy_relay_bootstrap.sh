0942eb67778dceace162d0d3bbefe7ff7516b0da
KheOps <kheops@ceops.eu>
2013-08-12 20:09:30 +0000

    Completely change (and temporarily break) the relay bootstrapping script:   * use a salt masterless method, where the bash script only installs and sets whats needed for the permanent salt-minion to run   * reason for this: should make more maintainable and coherent things installed on minions since we will use same SLS files for bootstrap and when under authority of the master   * we will use a temporary minion configuration file for the bootstrap where the bash script puts the specific info (ID, master hidden service, role)   * the permanent minion configuration file will include those grains through the masterless execution

--- remedy_relay_bootstrap.sh
+++ remedy_relay_bootstrap.sh
@@ -1,41 +1,59 @@
 #!/bin/bash
-# Run as root
+# Bootstrap script for Remedy Relays.
+# /!\ alpha status, experimental
+# Run as root - probably works only for Ubuntu systems for now
 
-# Install saltstack
-add-apt-repository ppa:saltstack/salt -y
-apt-get update -y
-apt-get install salt-minion -y
-apt-get upgrade -y
-apt-get install gcc make libevent-dev git socat autotools-dev autoconf automake libssl-dev -y
-
-# Compile and install tor
-git clone https://git.torproject.org/tor.git /tmp/tor
-cd /tmp/tor
-git checkout tor-0.2.3.25
-./autogen.sh && ./configure --disable-asciidoc && make && make install
-if [ -z "$(which tor)" ]; then
-  echo Tor install failed -- exiting
-  rm -fr /tmp/tor
+# Settings for newly created minion
+if [ -z "$REMEDY_MASTER_SERVICE_NAME" ]; then
+  # No reasonable default - exit
+  echo Variable REMEDY_MASTER_SERVICE_NAME not set, exiting
   exit 1
 fi
-
-# Configure and start tor
-echo -e 'SocksPort 9040\nRunAsDaemon 1\nPidFile /var/run/tor.pid' >/etc/torrc
-tor -f /etc/torrc
-
-if [ -z "$REMEDY_MASTER_SERVICE_NAME" ]; then
-  REMEDY_MASTER_SERVICE_NAME=ibraa6yfif5e6pbh.onion
+if [ -z "$REMEDY_MINION_ID" ]; then
+  # No reasonable default - exit
+  echo Variable REMEDY_MINION_ID not set, exiting
+  exit 1
 fi
 if [ -z "$REMEDY_ROLE_GRAIN" ]; then
+  # Reasonable default
   REMEDY_ROLE_GRAIN="relay_web"
 fi
 
+# Install salt-minion
+apt-get install python-software-properties -y
+add-apt-repository ppa:saltstack/salt -y
+wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | apt-key add -
+apt-get update
+apt-get install git salt-minion -y
+
+# Debian not so intelligently starts it automatically so let's stop it
+invoke-rc.d salt-minion stop
+
+# Prepare masterless run
+mkdir -p /tmp/remedy-bootstrap/srv
+if [ ! -d /tmp/remedy-bootstrap/srv ]; then
+  echo Could not create temporary directories to bootstrap the system
+  exit 1
+fi
+git clone https://github.com/ProjectRemedy/Remedy.git /tmp/Remedy
+cp -Rf /tmp/Remedy/salt/srv/salt /tmp/remedy-bootstrap/srv/
+
+echo -e 'id: '$REMEDY_MINION_ID'\ngrains:\n  remedy_role: '$REMEDY_ROLE_GRAIN'\n  master_hidden_service: '$REMEDY_MASTER_SERVICE_NAME \
+    >>/tmp/Remedy/salt/bootstrap/minion/minion
+
+# This should install and connect salt minion to the master through Tor
+salt-call -c /tmp/Remedy/salt/bootstrap/minion state.highstate
+
+rm -fr /tmp/Remedy /tmp/remedy-bootstrap
+
+# if the masterless execution of salt does its job correctly, we're theoretically done here.
+
+# following lines serve as a reminder for now (will be moved/deleted)
+# Configure and start tor
+#echo -e 'SocksPort 9040\nRunAsDaemon 1\nPidFile /var/run/tor.pid' >/etc/torrc
+#tor -f /etc/torrc
+
 # Start socat to forward to the Tor hidden service
-socat TCP4-LISTEN:4505,bind=127.0.0.1,fork,reuseaddr SOCKS4A:127.0.0.1:$REMEDY_MASTER_SERVICE_NAME:4505,socksport=9040 &
-socat TCP4-LISTEN:4506,bind=127.0.0.1,fork,reuseaddr SOCKS4A:127.0.0.1:$REMEDY_MASTER_SERVICE_NAME:4506,socksport=9040 &
+#socat TCP4-LISTEN:4505,bind=127.0.0.1,fork,reuseaddr SOCKS4A:127.0.0.1:$REMEDY_MASTER_SERVICE_NAME:4505,socksport=9040 &
+#socat TCP4-LISTEN:4506,bind=127.0.0.1,fork,reuseaddr SOCKS4A:127.0.0.1:$REMEDY_MASTER_SERVICE_NAME:4506,socksport=9040 &
 
-# Configure salt minion with grains and how to reach master and start minion
-echo -e 'master: 127.0.0.1\ngrains:\n  remedy_role: '$REMEDY_ROLE_GRAIN >/etc/salt/minion
-invoke-rc.d salt-minion restart
-
-rm -fr /tmp/tor
