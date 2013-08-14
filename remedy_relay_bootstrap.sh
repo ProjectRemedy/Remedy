eaa892fe0cbb7fe8dd77ac383343ed89dca6ad0f
KheOps <kheops@ceops.eu>
2013-08-14 18:20:07 +0000

    Move bootstrapping root_dir to /tmp/remedy-bootstrap, as previous path was yielding a "filename too long" by Salt's ZeroMQ.

--- remedy_relay_bootstrap.sh
+++ remedy_relay_bootstrap.sh
@@ -37,12 +37,13 @@ if [ ! -d /tmp/remedy-bootstrap/srv ]; then
 fi
 git clone https://github.com/ProjectRemedy/Remedy.git /tmp/Remedy
 cp -Rf /tmp/Remedy/salt/srv/salt /tmp/remedy-bootstrap/srv/
+cp -f /tmp/Remedy/salt/bootstrap/minion/minion /tmp/remedy-bootstrap/
 
 echo -e 'id: '$REMEDY_MINION_ID'\ngrains:\n  remedy_role: '$REMEDY_ROLE_GRAIN'\n  master_hidden_service: '$REMEDY_MASTER_SERVICE_NAME \
-    >>/tmp/Remedy/salt/bootstrap/minion/minion
+    >>/tmp/remedy-bootstrap/minion
 
 # This should install and connect salt minion to the master through Tor
-salt-call -c /tmp/Remedy/salt/bootstrap/minion state.highstate
+salt-call -c /tmp/remedy-bootstrap state.highstate
 
 rm -fr /tmp/Remedy /tmp/remedy-bootstrap
 
